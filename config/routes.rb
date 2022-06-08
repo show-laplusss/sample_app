35744b27ba2631b75544353de7f6aae691bd08b6
EC2 Default User <106435322+show->
2022-06-08 05:40:49 +0000

    app-3

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'lists/new'
   post 'lists' => 'lists#create'
-  get 'lists/index'
+  post 'lists' => 'lists#create'
+  get 'lists' => 'lists#index'
   get 'lists/show'
   get 'lists/edit'
   get '/top' => 'homes#top'
